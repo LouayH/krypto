ef1e32b583a8830550563e56c42d6e8aa23932a7
Louay Hamada <loauy.hamada@gmail.com>
2022-01-02 01:26:55 +0300

    Making of Loader component

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,6 +1,8 @@
 const Loader = () => {
   return (
-    <p>Loader</p>
+    <div className="flex justify-center items-center py-2">
+      <div className="animate-spin rounded-full h-[32px] w-[32px] border-b-2 border-red-700"></div>
+    </div>
   );
 }
 
